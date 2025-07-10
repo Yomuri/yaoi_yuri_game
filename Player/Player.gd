d80062bf2fb6912c8a8d141b548a0a8c26134073
Yomuri <deherro817@gmail.com>
2025-07-09 23:56:15 -0400

    Updated movement vectors

--- Player/Player.gd
+++ Player/Player.gd
@@ -8,7 +8,7 @@ func _physics_process(delta: float) -> void:
 	var z_movement : float = Input.get_action_strength("Backward") - Input.get_action_strength("Forward")
 	var rotate : float = Input.get_action_strength("Left") - Input.get_action_strength("Right")
 	rotation.y += rotate * rotate_speed * delta
-	var direction : Vector3 = Vector3(0,0,1).rotated(Vector3(0,1,0), rotation.y)
+	var direction : Vector3 = Vector3.FORWARD.rotated(Vector3.UP, rotation.y)
 	var motion : Vector3 = direction * z_movement * delta * move_speed
 	move_and_collide(motion)
 	pass
