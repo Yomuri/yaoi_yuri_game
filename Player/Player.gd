e9233c2b812f3812916563ce1440b1baa71a20b4
Yomuri <deherro817@gmail.com>
2025-07-10 01:05:27 -0400

    Fixed inverted forward movement

--- Player/Player.gd
+++ Player/Player.gd
@@ -5,7 +5,7 @@ class_name Player
 @export var rotate_speed : float = 3.0
 
 func _physics_process(delta: float) -> void:
-	var z_movement : float = Input.get_action_strength("Backward") - Input.get_action_strength("Forward")
+	var z_movement : float = Input.get_action_strength("Forward") - Input.get_action_strength("Backward")
 	var rotate : float = Input.get_action_strength("Left") - Input.get_action_strength("Right")
 	rotation.y += rotate * rotate_speed * delta
 	var direction : Vector3 = Vector3.FORWARD.rotated(Vector3.UP, rotation.y)
